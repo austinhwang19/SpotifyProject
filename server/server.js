9f27ef2550a6f7c9a3e31ad304b4cf8689a1bf9a
Austin Hwang <austin@Austins-MBP.attlocal.net>
2022-02-06 14:50:30 -0500

    finished:

--- server/server.js
+++ server/server.js
@@ -1,58 +1,70 @@
-const express = require('express');
-const SpotifyWebApi = require('spotify-web-api-node');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+require('dotenv').config()
+console.log(process.env)
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const lyricsFinder = require("lyrics-finder")
+const SpotifyWebApi = require("spotify-web-api-node")
 
-const app = express();
+const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-
-app.post('/login', (req, res) => {
-    const code = req.body.code;
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: 'e37916d70d074851b71315a78030cbac',
-        clientSecret: '79775350c5324bd19a9cfdb823e4210b'
-    })
-
-    spotifyApi.authorizationCodeGrant(code)
-        .then(data => {
-            res.json({
-                accessToken: data.body.access_token,
-                refresToken: data.body.refresh_token,
-                expiresIn: data.body.expires_in,
-            })
-        })
-        .catch(err => {
-            console.log(err)
-            res.sendStatus(400)
-        })
-})
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.post('/refresh', (req, res) => {
-    const refreshToken = req.body.refreshToken
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: 'e37916d70d074851b71315a78030cbac',
-        clientSecret: '79775350c5324bd19a9cfdb823e4210b',
-        refreshToken
-    })
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  })
 
-    spotifyApi.refreshAccessToken().then(
-        (data) => {
-            res.json({
-                accessToken: data.body.accessToken,
-                expiresIn: data.body.expiresIn
-            })
-            // Save the access token so that it's used in future calls
-            // spotifyApi.setAccessToken(data.body['access_token']);
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
     })
     .catch(err => {
-        console.log(err)
-        res.sendStatus(400)
+      console.log(err)
+      res.sendStatus(400)
     })
 })
 
-app.listen(3001);
-console.log('listening on port 3001');
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  console.log("BIG TESTING VIBES")
+  console.log(process.env.CLIENT_ID)
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+  res.json({ lyrics })
+})
+
+
+app.listen(3001)
